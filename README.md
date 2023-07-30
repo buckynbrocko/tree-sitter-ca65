6f6054a7cc5953751192e2067a6bace33c184fbe
buckynbrocko <77247638+buckynbrocko@users.noreply.github.com>
2023-07-30 17:59:40 -0500

    Pruning directories

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # tree-sitter-ca65
 A tree-sitter grammar for the ca65 assembler of the cc65 development package.
+
+# Development
+
+## Prerequisites
+
+- Node.js, npm
+- make
+- Emscripten
+
+## Quick Start
+
+```shell
+make init
+```
--- target/debug/deps/cc-7314326891f102ee.d
+++
@@ -1,8 +0,0 @@
-/Users/buckyallen/Documents (Local)/Local Repos/cc65-vscode-ext/tree-sitter-ca65/target/debug/deps/cc-7314326891f102ee.rmeta: /Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs /Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs
-
-/Users/buckyallen/Documents (Local)/Local Repos/cc65-vscode-ext/tree-sitter-ca65/target/debug/deps/libcc-7314326891f102ee.rlib: /Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs /Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs
-
-/Users/buckyallen/Documents (Local)/Local Repos/cc65-vscode-ext/tree-sitter-ca65/target/debug/deps/cc-7314326891f102ee.d: /Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs /Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs
-
-/Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs:
-/Users/buckyallen/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs:
